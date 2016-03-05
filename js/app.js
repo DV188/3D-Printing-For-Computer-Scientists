bb0744dd6b17c0939ac6404b0568c235f0e2e589
Danen Van De Ven <danenevandeven@gmail.com>
2016-03-05 09:25:27 -0500

    Rearranging files for better directory structure.

--- js/app.js
+++ js/app.js
@@ -10,39 +10,39 @@ var honoursApp = angular.module('honoursApp', [
 
 honoursApp.config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/introduction', {
-        templateUrl: 'partials/introduction.html'
+        templateUrl: 'partials/openscad/introduction.html'
     }).when('/getting_started', {
-        templateUrl: 'partials/getting_started.html'
+        templateUrl: 'partials/openscad/getting_started.html'
     }).when('/programmers_option', {
-        templateUrl: 'partials/programmers_option.html'
+        templateUrl: 'partials/openscad/programmers_option.html'
     }).when('/openscad_basics', {
-        templateUrl: 'partials/openscad_basics.html'
+        templateUrl: 'partials/openscad/openscad_basics.html'
     }).when('/3d_models_in_openscad', {
-        templateUrl: 'partials/3d_models_in_openscad.html'
+        templateUrl: 'partials/openscad/3d_models_in_openscad.html'
     }).when('/2d_models_in_openscad', {
-        templateUrl: 'partials/2d_models_in_openscad.html'
+        templateUrl: 'partials/openscad/2d_models_in_openscad.html'
     }).when('/importing_cad_drawings', {
-        templateUrl: 'partials/importing_cad_drawings.html'
+        templateUrl: 'partials/openscad/importing_cad_drawings.html'
     }).when('/manipulating_your_model', {
-        templateUrl: 'partials/manipulating_your_model.html'
+        templateUrl: 'partials/openscad/manipulating_your_model.html'
     }).when('/transforming_your_model', {
-        templateUrl: 'partials/transforming_your_model.html'
+        templateUrl: 'partials/openscad/transforming_your_model.html'
     }).when('/combining_objects', {
-        templateUrl: 'partials/combining_objects.html'
+        templateUrl: 'partials/openscad/combining_objects.html'
     }).when('/familiar_programming_functions', {
-        templateUrl: 'partials/familiar_programming_functions.html'
+        templateUrl: 'partials/openscad/familiar_programming_functions.html'
     }).when('/list_comprehensions', {
-        templateUrl: 'partials/list_comprehensions.html'
+        templateUrl: 'partials/openscad/list_comprehensions.html'
     }).when('/functions_and_modules', {
-        templateUrl: 'partials/functions_and_modules.html'
+        templateUrl: 'partials/openscad/functions_and_modules.html'
     }).when('/debugging', {
-        templateUrl: 'partials/debugging.html'
+        templateUrl: 'partials/openscad/debugging.html'
     }).when('/external_files', {
-        templateUrl: 'partials/external_files.html'
+        templateUrl: 'partials/openscad/external_files.html'
     }).when('/stereolithography', {
-        templateUrl: 'partials/stereolithography.html'
+        templateUrl: 'partials/openscad/stereolithography.html'
     }).when('/slicer', {
-        templateUrl: 'partials/slicer.html'
+        templateUrl: 'partials/slicer/slicer.html'
     }).otherwise({
         redirectTo: '/introduction'
     });
